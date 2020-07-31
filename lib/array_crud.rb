9bf2d2d8d2897e96e143e6a9497ea1f50556a85b
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-07-31 20:08:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,7 @@ puts create_an_array
 def add_element_to_end_of_array
   ["wow", "I", "am", "learning" ]
 end
-puts add_element_to_end_of_array.push maths 
+puts add_element_to_end_of_array.push(maths)
 
 def add_element_to_start_of_array(array, element)
   
